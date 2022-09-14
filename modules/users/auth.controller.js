38ed8d4f09d67d2a9f40250c2a2a407ce2b1b0da
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2022-09-14 07:10:36 -0700

    Add files via upload

--- modules/users/auth.controller.js
+++ modules/users/auth.controller.js
@@ -1,4 +1,21 @@
-const User = require("./user.model")
+const User = require("./user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+const generateToken = (user)=>{
+    const token = jwt.sign({id: user._id, email: user.email},
+        "5faaf12d1655c7b572843e87f41050f6a15becdaf0eebfbad8cb3d513d993733",
+       { expiresIn: "1h" }      
+
+         );
+         return{
+            token,
+            user,
+         };
+};
+
+
 exports.register = async (req, res)=>{
     // Email
     // get variable from the requested body
@@ -10,9 +27,34 @@ exports.register = async (req, res)=>{
         return res.status(400).json({error:"Email Already in Use"})
     }
 
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 12)
+    
 
     // create User 
-    const user = await User.create({...req.body});
-    res.status(201).json({user});
+    const user = await User.create({...req.body, password: hashedPassword});
 
-}
+    // generate token
+   const token= generateToken(user)
+    res.status(201).json({token});
+
+
+};
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+
+    let user = await User.findOne({email});
+    if (!user) {
+        return res.status(400).json({msg: "Invalid credential"});
+
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({msg: "Invalid credential"})
+    }
+
+    const token= generateToken(user)
+    res.status(201).json({token});
+};
+
